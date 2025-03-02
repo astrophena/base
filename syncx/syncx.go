fe08c85e6e40a49f2c38bb78c4cf04fd2fd8f6b3
Ilya Mateyko <me@astrophena.name>
2025-03-02 16:00:04 +0300

    syncx: simplify LimitedWaitGroup docs

--- syncx/syncx.go
+++ syncx/syncx.go
@@ -51,23 +51,21 @@ func (l *Lazy[T]) GetErr(f func() (T, error)) (T, error) {
 	return l.val, l.err
 }
 
-// LimitedWaitGroup is a version of [sync.WaitGroup] that limits the
-// number of concurrently working goroutines by using a buffered channel
-// as a semaphore.
+// LimitedWaitGroup is a [sync.WaitGroup] that limits the number of concurrently
+// working goroutines.
 type LimitedWaitGroup struct {
 	wg      sync.WaitGroup
 	workers chan struct{}
 }
 
-// NewLimitedWaitGroup returns a new LimitedWaitGroup that limits the number of
-// concurrently working goroutines to limit.
+// NewLimitedWaitGroup returns a new [LimitedWaitGroup].
 func NewLimitedWaitGroup(limit int) *LimitedWaitGroup {
 	return &LimitedWaitGroup{
 		workers: make(chan struct{}, limit),
 	}
 }
 
-// Add increments the counter of the LimitedWaitGroup by the specified delta.
+// Add increments the counter of the [LimitedWaitGroup] by the specified delta.
 // It blocks if the number of active goroutines reaches the concurrency limit.
 func (lwg *LimitedWaitGroup) Add(delta int) {
 	for range delta {
@@ -76,12 +74,12 @@ func (lwg *LimitedWaitGroup) Add(delta int) {
 	}
 }
 
-// Done decrements the counter of the LimitedWaitGroup by one and releases a slot
-// in the semaphore, allowing another goroutine to start.
+// Done decrements the counter of the [LimitedWaitGroup] by one and releases a
+// slot, allowing another goroutine to start.
 func (lwg *LimitedWaitGroup) Done() {
 	<-lwg.workers
 	lwg.wg.Done()
 }
 
-// Wait blocks until the counter of the LimitedWaitGroup becomes zero.
+// Wait blocks until the counter of the [LimitedWaitGroup] becomes zero.
 func (lwg *LimitedWaitGroup) Wait() { lwg.wg.Wait() }
