c538e2cf69504bb6e867c9d0e5b87dd6b6ecbb7d
Ilya Mateyko <me@astrophena.name>
2025-04-19 17:33:35 +0300

    syncx: refactor

--- syncx/syncx.go
+++ syncx/syncx.go
@@ -8,9 +8,10 @@ package syncx
 import "sync"
 
 // Protect wraps T into [Protected].
-func Protect[T any](val T) *Protected[T] { return &Protected[T]{val: val} }
+func Protect[T any](val T) Protected[T] { return Protected[T]{val: val} }
 
 // Protected provides synchronized access to a value of type T.
+// It should not be copied.
 type Protected[T any] struct {
 	mu  sync.RWMutex
 	val T
@@ -65,6 +66,16 @@ func NewLimitedWaitGroup(limit int) *LimitedWaitGroup {
 	}
 }
 
+// Go starts a new goroutine that executes f.
+// It blocks if the number of active goroutines reaches the concurrency limit.
+func (lwg *LimitedWaitGroup) Go(f func()) {
+	lwg.Add(1)
+	go func() {
+		defer lwg.Done()
+		f()
+	}()
+}
+
 // Add increments the counter of the [LimitedWaitGroup] by the specified delta.
 // It blocks if the number of active goroutines reaches the concurrency limit.
 func (lwg *LimitedWaitGroup) Add(delta int) {
@@ -83,3 +94,40 @@ func (lwg *LimitedWaitGroup) Done() {
 
 // Wait blocks until the counter of the [LimitedWaitGroup] becomes zero.
 func (lwg *LimitedWaitGroup) Wait() { lwg.wg.Wait() }
+
+// Map is a generic version of [sync.Map].
+type Map[K, V comparable] struct{ m sync.Map }
+
+// Load is [sync.Map.Load].
+func (m *Map[K, V]) Load(key K) (value V, ok bool) {
+	val, ok := m.m.Load(key)
+	if !ok {
+		return value, false
+	}
+	return val.(V), true
+}
+
+// Store is [sync.Map.Store].
+func (m *Map[K, V]) Store(key K, value V) { m.m.Store(key, value) }
+
+// LoadOrStore is [sync.Map.LoadOrStore].
+func (m *Map[K, V]) LoadOrStore(key K, value V) (actual V, loaded bool) {
+	v, loaded := m.m.LoadOrStore(key, value)
+	return v.(V), loaded
+}
+
+// LoadAndDelete is [sync.Map.LoadAndDelete].
+func (m *Map[K, V]) LoadAndDelete(key K) (value V, loaded bool) {
+	v, loaded := m.m.LoadAndDelete(key)
+	return v.(V), loaded
+}
+
+// Delete is [sync.Map.Delete].
+func (m *Map[K, V]) Delete(key K) { m.m.Delete(key) }
+
+// Range is [sync.Map.Range].
+func (m *Map[K, V]) Range(f func(key K, value V) bool) {
+	m.m.Range(func(key, value any) bool {
+		return f(key.(K), value.(V))
+	})
+}
