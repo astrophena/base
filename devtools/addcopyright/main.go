46f9c2f927ed5872929621a6347ec5d0d5f700eb
Ilya Mateyko <me@astrophena.name>
2025-07-27 14:51:13 +0300

    devtools/addcopyright: move config to .addcopyright.txtar (#30)

--- devtools/addcopyright/main.go
+++ devtools/addcopyright/main.go
@@ -2,48 +2,32 @@
 // Use of this source code is governed by the ISC
 // license that can be found in the LICENSE.md file.
 
-// Addcopyright adds copyright header to each Go file.
 package main
 
 import (
 	"bytes"
+	"context"
+	"encoding/json"
+	"flag"
 	"fmt"
 	"io/fs"
-	"log"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"go.astrophena.name/base/cli"
 	"go.astrophena.name/base/devtools/internal"
+	"go.astrophena.name/base/txtar"
 )
 
-var templates = map[string]string{
-	".go": `// © %d Ilya Mateyko. All rights reserved.
-// Use of this source code is governed by the ISC
-// license that can be found in the LICENSE.md file.
-
-`,
+type config struct {
+	exclusions []string
+	headers    map[string]string
+	templates  map[string]string
 }
 
-var headers = map[string]string{
-	".go": `// ©`,
-}
-
-var exclusions = []string{
-	"internal/hashfs/hashfs.go",
-	"internal/hashfs/hashfs_test.go",
-	// Based on Go's telemetry code.
-	"internal/unionfs/unionfs.go",
-	"internal/unionfs/unionfs_test.go",
-	// Based on Go's standard library code.
-	"txtar/txtar.go",
-	// Based on Tailscale code.
-	"web/debug.go",
-	"web/debug_test.go",
-}
-
-func isExcluded(path string) bool {
-	for _, ex := range exclusions {
+func (cfg *config) isExcluded(path string) bool {
+	for _, ex := range cfg.exclusions {
 		if strings.HasSuffix(path, ex) {
 			return true
 		}
@@ -51,23 +35,69 @@ func isExcluded(path string) bool {
 	return false
 }
 
-func main() {
+func parseConfig() (*config, error) {
+	cfg := &config{
+		headers:   make(map[string]string),
+		templates: make(map[string]string),
+	}
+
+	ar, err := txtar.ParseFile(".addcopyright.txtar")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, f := range ar.Files {
+		if f.Name == "exclusions.json" {
+			if err := json.Unmarshal(f.Data, &cfg.exclusions); err != nil {
+				return nil, err
+			}
+		}
+		ext := filepath.Ext(f.Name)
+		if strings.HasPrefix(f.Name, "template") {
+			cfg.templates[ext] = string(f.Data)
+		}
+		if strings.HasPrefix(f.Name, "header") {
+			cfg.headers[ext] = strings.TrimSuffix(string(f.Data), "\n")
+		}
+	}
+
+	return cfg, nil
+}
+
+func main() { cli.Main(new(app)) }
+
+type app struct {
+	dry bool
+}
+
+func (a *app) Flags(fs *flag.FlagSet) {
+	fs.BoolVar(&a.dry, "dry", false, "Print the files that would have a copyright header added, without making changes.")
+}
+
+func (a *app) Run(ctx context.Context) error {
 	internal.EnsureRoot()
 
-	if err := filepath.WalkDir(".", func(path string, d fs.DirEntry, err error) error {
+	env := cli.GetEnv(ctx)
+
+	cfg, err := parseConfig()
+	if err != nil {
+		return err
+	}
+
+	return filepath.WalkDir(".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 
-		if d.IsDir() || isExcluded(path) {
+		if d.IsDir() || cfg.isExcluded(path) {
 			return nil
 		}
 		ext := filepath.Ext(path)
-		tmpl, ok := templates[ext]
+		tmpl, ok := cfg.templates[ext]
 		if !ok {
 			return nil
 		}
-		header, ok := headers[ext]
+		header, ok := cfg.headers[ext]
 		if !ok {
 			return nil
 		}
@@ -94,8 +124,10 @@ func main() {
 		buf.WriteString(hdr)
 		buf.Write(content)
 
+		if a.dry {
+			env.Logf("Would add copyright header to file %s:\n%s", path, hdr)
+			return nil
+		}
 		return os.WriteFile(path, buf.Bytes(), 0o644)
-	}); err != nil {
-		log.Fatal(err)
-	}
+	})
 }
